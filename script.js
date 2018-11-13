661ce55108d48a6e371f057de794ee2eeb082a41
Lucassion <lucasinventor@gmail.com>
2018-11-12 22:05:34 -0200

    Exercicio

---
+++ script.js
@@ -0,0 +1,42 @@
+// Exercicio: inverter os caractéres de uma string
+
+// Primeira forma
+
+// function inverter (scr){
+//     let saida = '';
+
+//     for (let i = scr.length - 1; i >= 0; i--){
+//     saida += scr[i]
+//     }
+//     return saida;
+// }
+
+
+// Segunda forma
+
+// function reverter (contrario) {
+//     return contrario.slip('').reverse().join('');
+// }
+
+
+// Terceira forma
+// function mudarordem ()
+
+/*
+Criar 3 funções, onde:
+
+A primeira função recebe o nome e o conjunto de notas;
+A segunda função diz qual é a media do aluno
+A terceira função diz se o aluno foi aprovado ou não, considerando que a média é 6.
+
+*/
+
+function nomeConjuntoNotas (nome, notas) {
+    console.log(nome);
+    consolo.log(notas);
+}
+
+let nome ='Lucas';
+let notas = [10, 5, 6 ,8];
+
+nomeConjuntoNotas(nome, notas);
